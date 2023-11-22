019d2d4c0031b3b6b7717bbabeb199282aeea295
RaxmatullayevAzamjon <raxmatullayevazamjon025@gmail.com>
2023-11-22 15:30:44 +0500

    Maxsulot jadvali qo'shildi

--- asosiy/models.py
+++ asosiy/models.py
@@ -6,3 +6,13 @@ class Bolim(models.Model):
 
     def __str__(self):
         return self.nom
+
+class Mahsulot(models.Model):
+    nom = models.CharField(max_length=30)
+    brend = models.CharField(max_length=20)
+    narx = models.CharField(max_length=30)
+    bolim = models.ForeignKey(Bolim, on_delete=models.CASCADE)
+    batafsil = models.CharField(max_length=30, blank=True, null=True)
+
+    def str(self):
+        return self.nom
