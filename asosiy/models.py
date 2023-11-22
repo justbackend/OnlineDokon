fd02ba50bcbcf3a496cca3106f6b5f32cce0184d
alijanovashukrona <shukronaalijanova@gmail.com>
2023-11-22 15:18:52 +0500

    Bolim jadvali qoshildi

--- asosiy/models.py
+++ asosiy/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Bolim(models.Model):
+    nom=models.CharField(max_length=100)
+    rasm=models.FileField(null=True)
+
+    def __str__(self):
+        return self.nom
