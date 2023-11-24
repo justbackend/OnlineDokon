2ee3693287fb0296610c77f2b0ba195b8dcc1dc3
justbackend <izzatullaev2001abror@gmail.com>
2023-11-24 15:08:28 +0500

    8 vazifani bajardim

--- asosiy/views.py
+++ asosiy/views.py
@@ -1,3 +1,23 @@
+from .serializers import *
+from .models import *
+from rest_framework.views import APIView
 from django.shortcuts import render
-
+from rest_framework.response import Response
 # Create your views here.
+
+
+
+class MahsulotQidiruvApi(APIView):
+    def get(self, request):
+        mahsulotlar = Mahsulot.objects.all()
+        qidiruv_soz = request.query_params.get("brend")
+        if qidiruv_soz:
+            mahsulotlar = Mahsulot.objects.filter(brend__contains=qidiruv_soz)
+        serializer = MaxsulotSerializers(mahsulotlar, many=True)
+        return Response(serializer.data)
+
+class MahsulotGetApi(APIView):
+    def get(self, request, pk):
+        model = Mahsulot.objects.filter(nom__contains=pk)
+        serializer = MaxsulotSerializers(model, many=True)
+        return Response(serializer.data)
