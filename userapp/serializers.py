c5b81ec327b93bf885dd34a343465716c025cb74
ahmedofweb <abdulxamidweb@gmail.com>
2023-11-24 14:13:56 +0300

    userapp serializeriga Account jadvali uchun serializer yozildi

---
+++ userapp/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import *
+
+class AccountSerializer(ModelSerializer):
+    class Meta:
+        model = Account
+        fields = "__all__"
